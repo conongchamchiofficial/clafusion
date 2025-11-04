245d43dd686a46a746f98aded2a3c544181d50c4
conongchamchiofficial <conongchamchi.official@gmail.com>
2025-11-04 18:00:46 +0800

    Update fuse_mlp_p_split.sh

--- sh/fuse_mlp_p_split.sh
+++ sh/fuse_mlp_p_split.sh
@@ -10,6 +10,6 @@ do
     do
         STEP=$(echo "$i / 10" | bc -l)
         echo "----- step = $STEP"
-        python split_main.py --gpu-id 0 --model-name mlpnet --n-epochs 10 --save-result-file seed_$seed/${BALANCE}_constraint.csv --exact --correction --ground-metric euclidean --activation-histograms --activation-mode raw --geom-ensemble-type acts --act-num-samples $N_SAMPLES --ground-metric-normalize none --activation-seed 21 --prelu-acts --load-models $DATA_PATH --ckpt-type final --past-correction --not-squared --dist-normalize --skip-last-layer --load-personalized-datasets $DATA_PATH --balance-method $BALANCE --free-end-layers 1 --prediction-wts --ensemble-step $STEP --relu-approx-method sum --to-download
+        python split_main.py --gpu-id 0 --model-name mlpnet --n-epochs 10 --save-result-file seed_$seed/${BALANCE}_constraint.csv --exact --correction --ground-metric euclidean --activation-histograms --activation-mode raw --geom-ensemble-type acts --act-num-samples $N_SAMPLES --ground-metric-normalize none --activation-seed 21 --prelu-acts --load-models $DATA_PATH --ckpt-type final --past-correction --not-squared --dist-normalize --skip-last-layer --load-personalized-datasets $DATA_PATH --balance-method $BALANCE --free-end-layers 1 --prediction-wts --ensemble-step $STEP --relu-approx-method sum
     done
 done
